9fcc720111b35a8a0c078d2b8ab8151742797b04
Aleksandar Gosevski <gosevski@vast.com>
2018-11-27 13:37:13 -0600

    Files should be in their right directories

--- currentWork/readData/readData.ino
+++ currentWork/readData/readData.ino
@@ -1,9 +1,12 @@
 #include <Wire.h>
 #include <LiquidCrystal.h>
 
-// unnecessary comment
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+String temp = "";
+String temp2 = "";
+int playerScore1 = 0;
+int playerScore2 = 0;
 
 void setup() {
   lcd.begin(16, 2);
@@ -12,24 +15,35 @@ void setup() {
   Serial.begin(9600);  // start serial for output
 }
 
+
 void loop() {
-  Wire.requestFrom(8, 6);    // request 6 bytes from slave device #8
+
+  Wire.requestFrom(7, 4);    // request 6 bytes from slave device #8
   // lcd.setCursor(0, 0);
 
   while (Wire.available()) { // slave may send less than requested
     char c = Wire.read(); // receive a byte as character
-    lcd.print(c);
-    Serial.print(c);         // print the character
+    //lcd.print(c);
+    temp += c;
+    Serial.print(c);
+    // print the character
   }
-
+  lcd.print(temp);
   // lcd.setCursor(0, 16);
 
-  Wire.requestFrom(7, 6);
-   while (Wire.available()) { // slave may send less than requested
+  Wire.requestFrom(8, 4);
+  while (Wire.available()) { // slave may send less than requested
     char c = Wire.read(); // receive a byte as character
-    lcd.print(c);
+    //lcd.print(c);
+    temp2 += c;
     Serial.print(c);         // print the character
   }
-
+  lcd.print(" ");
+  lcd.print(temp2);
+  lcd.print(" ");
+  temp = "";
+  temp2 = "";
   delay(500);
+  
+
 }
